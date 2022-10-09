ed6d1050adb2794a2619f3f1234db2aa4bb5cd54
flyc <yayayahahahaooii@gmail.com>
2022-10-10 02:55:46 +0800

    update masterhouse config

--- masterHouse.js
+++ masterHouse.js
@@ -1,4 +1,8 @@
 const MasterHouse = require('MasterHouse')
-const masterHouse = new MasterHouse({ workerNumber: 10, basicDelay: 500, randomDelay: 500 })
+const masterHouse = new MasterHouse({
+  workerNumber: 1,
+  basicDelay: 1000,
+  randomDelay: 500,
+})
 
 module.exports = { masterHouse }
